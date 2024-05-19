04822fd2d08f2f86f413a951e91a08a96061eb5e
ohcrums <149456208+ohcrums@users.noreply.github.com>
2024-05-19 13:57:33 -0500

    experiments

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -1,6 +1,7 @@
 package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -17,22 +18,32 @@ public class HelloController {
 
     // Handles requests of the form /hello?name=LaunchCode
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
-    @ResponseBody
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
+    public String helloWithQueryParam(@RequestParam String name, Model model) {
+        String greetingString = "Hello " + name + "!";
+        model.addAttribute("greeting", greetingString);
+        return "hello";
     }
 
     // Handles requests of the form /hello/LaunchCode
     @GetMapping("hello/{name}")
-    @ResponseBody
-    public String helloWithPathParam(@PathVariable String name) {
-        return "Hello, " + name + "!";
+    public String helloWithPathParam(@PathVariable String name, Model model) {
+        model.addAttribute("greeting", "Hello, " + name + "!");
+        return "hello";
     }
 
     // /hello/form
     @GetMapping("form")
     public String helloForm() {
         return "form";
+    }
+
+    // conditional loop
+    @GetMapping("hello-names")
+    public String helloNames(Model model) {
+        String[] nameList = new String[]{"Jasmine", "Zachary", "Alfredo", "Tyson Fury"};
+        model.addAttribute("names", nameList);
+
+        return "hello-list";
     }
 
 }
